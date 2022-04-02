d6b1341a1df356686b2eb275ae16456f7671dd24
Laurah <laurachege9@gmail.com>
2022-04-02 11:02:57 +0300

    Commit to master

--- app/src/main/java/com/example/donationproject/LoginActivity.java
+++ app/src/main/java/com/example/donationproject/LoginActivity.java
@@ -7,19 +7,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private Button registerButton;
+    private TextView backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        registerButton = findViewById(R.id.registerButton);
+        backButton = findViewById(R.id.backButton);
 
-        registerButton.setOnClickListener(new View.OnClickListener() {
+        backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(
