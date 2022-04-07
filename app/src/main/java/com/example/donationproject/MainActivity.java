cdc091a171a9d7139a73a223d38045290e00795c
Laurah <laurachege9@gmail.com>
2022-04-07 21:36:43 +0300

    Commit to master

--- app/src/main/java/com/example/donationproject/MainActivity.java
+++ app/src/main/java/com/example/donationproject/MainActivity.java
@@ -1,23 +1,42 @@
 package com.example.donationproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity extends AppCompatActivity {
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
 
     private DrawerLayout drawerLayout;
     private Toolbar toolbar;
     private NavigationView nav_view;
+
+    private CircleImageView nav_profile_image;
+    private TextView nav_fullname, nav_email, nav_donationtype, nav_type;
+
+    private DatabaseReference userRef;
 
 
     @Override
@@ -38,8 +57,56 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
+        nav_view.setNavigationItemSelectedListener(this);
 
+        nav_profile_image = nav_view.getHeaderView(0).findViewById(R.id.nav_user_image);
+        nav_fullname = nav_view.getHeaderView(0).findViewById(R.id.nav_user_fullname);
+        nav_email = nav_view.getHeaderView(0).findViewById(R.id.nav_user_email);
+        nav_donationtype = nav_view.getHeaderView(0).findViewById(R.id.nav_user_donationtype);
+        nav_type = nav_view.getHeaderView(0).findViewById(R.id.nav_user_type);
 
+        userRef = FirebaseDatabase.getInstance().getReference().child("users").child(
+                FirebaseAuth.getInstance().getCurrentUser().getUid()
+        );
 
+        userRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    String name = snapshot.child("name").getValue().toString();
+                    nav_fullname.setText(name);
+
+                    String email = snapshot.child("email").getValue().toString();
+                    nav_email.setText(name);
+
+                    String donationtype = snapshot.child("donationtype").getValue().toString();
+                    nav_donationtype.setText(name);
+
+                    String type = snapshot.child("type").getValue().toString();
+                    nav_type.setText(name);
+
+                    String imageUrl = snapshot.child("profilepictureurl").getValue().toString();
+                    Glide.with(getApplicationContext()).load(imageUrl).into(nav_profile_image);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.profile:
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(intent);
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
